2229d77a4dde3e23629a36eeea88a784acac59c0
114886 <1148864234@qq.com>
2022-02-12 19:21:31 +0800

    aside css 调节

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,11 +2,11 @@
   <div>
     <Topnav />
     <div class="content">
-       <aside>
+      <aside>
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
+            <router-link to="/doc/swich">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -23,10 +23,28 @@
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 export default {
   components: { Topnav },
 };
 </script>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
