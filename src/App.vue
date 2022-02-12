647a2d7f6d731005b8899190b00060733b963bb2
114886 <1148864234@qq.com>
2022-02-12 17:45:42 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hh</div>
+  <Frank />
+</template>
+
+<script>
+import Frank from "./components/Frank.vue";
+
+export default {
+  name: "App",
+  components: {
+    Frank,
+  },
+};
+</script>
