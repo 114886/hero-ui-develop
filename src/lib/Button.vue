735a3a1bc98463c61955fe353b748c40bbc1bc5e
114886 <1148864234@qq.com>
2022-02-13 18:02:34 +0800

    button基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,23 +1,51 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
-      <slot></slot>
-    </button>
-  </div>
+  <button class="gulu-button" :class="{ [`theme-${theme}`]: theme }">
+    <slot />
+  </button>
 </template>
-
-<script>
+<script lang="ts">
 export default {
-  inheritAttrs: false, //是否默认继承属性
-  props:{},
-  setup(props, context) {
-    const { size, ...rest } = context.attrs;
-    return { size, rest };
-    //attrs包括继承来的东西，使用上面
-    // ...rest 是一个剩余操作符，就是除了size之外的 rest可以改任何名字
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
   },
 };
 </script>
-
 <style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>
