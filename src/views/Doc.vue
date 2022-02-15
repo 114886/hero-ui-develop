4722dc44f5b7a9fafeba1bc7eecbe1605ac6c735
114886 <1148864234@qq.com>
2022-02-15 18:06:20 +0800

    创建路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,18 @@
     <Topnav toggleMenuButtonVisible class="nav" />
     <div class="content">
       <aside v-if="menuVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -47,29 +59,36 @@ export default {
   display: flex;
   flex-direction: column;
   height: 100vh;
+
   > .nav {
     flex-shrink: 0;
   }
+
   > .content {
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
+
     @media (max-width: 500px) {
       padding-left: 0;
     }
   }
 }
+
 .content {
   display: flex;
+
   > aside {
     flex-shrink: 0;
   }
+
   > main {
     flex-grow: 1;
     padding: 16px;
     background: white;
   }
 }
+
 aside {
   background: lightblue;
   width: 150px;
@@ -79,15 +98,18 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
+
   > h2 {
     margin-bottom: 4px;
   }
+
   > ol {
     > li {
       padding: 4px 0;
     }
   }
 }
+
 main {
   overflow: auto;
 }
